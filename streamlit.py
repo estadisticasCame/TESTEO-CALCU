72f633aecf603db843fa97cc4750e7380a5fd9e8
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2024-10-10 10:42:02 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -279,6 +279,43 @@ if (st.session_state.submit_button == True):
         with st.spinner("Cargando . . ."): 
 
             df = pd.read_csv("Datos/Datos.csv")
+
+            tasas_por_provincia = df[["Provincia", "Tasa IIBB"]].drop_duplicates().reset_index(drop=True)
+            tasas_por_provincia["Tasa IIBB"] = (tasas_por_provincia["Tasa IIBB"] * 100 ).round(1).astype(str).str.replace(".",",") + "%"
+            
+            # Desplegable con scroll
+            provincia_seleccionada = st.selectbox("Selecciona una provincia", tasas_por_provincia["Provincia"], index=0, max_options=5)
+            
+            # Mostrar tabla completa
+            st.write("Tasas de IIBB por provincia:")
+            
+            # Aplicar estilo con HTML
+            table_style = """
+                <style>
+                .styled-table {
+                    width: 100%;
+                    border-collapse: collapse;
+                }
+                .styled-table thead tr {
+                    background-color: #007bff;
+                    color: white;
+                    text-align: left;
+                }
+                .styled-table th, .styled-table td {
+                    padding: 12px 15px;
+                }
+                .styled-table tbody tr {
+                    border-bottom: 1px solid #dddddd;
+                }
+                </style>
+            """
+            st.markdown(table_style, unsafe_allow_html=True)
+            
+            # Mostrar la tabla con formato
+            st.markdown(
+                tasas_por_provincia.to_html(index=False, classes="styled-table"), 
+                unsafe_allow_html=True
+            )
             # Filtramos según los valores indicados
             filtros = (df["Cuota Simple"] == st.session_state.programa_seleccionado) & (df["Provincia"] == st.session_state.provincia_seleccionada)
             df = df[filtros]
